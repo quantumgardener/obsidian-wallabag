faf5bfc3eb9ee9b7027d4935916709a5afd81019
jason-mehmel <me@jasonmehmel.com>
2025-10-10 17:41:36 -0600

    this update fixes the tag list property and includes a note template

---
+++ Wallabag-Template.md
@@ -0,0 +1,19 @@
+---
+tags: {{tags}} 
+created: {{created_at}}
+updated: {{updated_at}}
+published: {{published_at}}
+title: {{article_title}}
+author: {{published_by_list}}
+publication-year:
+recommender:
+id: {{id}}
+wallabag-link: {{wallabag_link}}
+Original-link: {{original_link}}
+---
+  
+## Annotations
+{{annotations}}
+
+## Content
+{{content}}
--- src/settings/WallabagSettingTab.ts
+++ src/settings/WallabagSettingTab.ts
@@ -174,11 +174,13 @@ export class WallabagSettingTab extends PluginSettingTab {
       .setDesc(
         sanitizeHTMLToDom(
           'Determines how the tags will be populated in the created note. <br>' +
+            'List: the current Obsidian standard for hashtags existing in the properties list. <br>' +          
             'CSV: Comma-separeted tags e.g. <code>tag1, tag2, tag3</code> <br>' +
             'Hashtags: Space-separeted hashtags<code>#tag1 #tag2 #tag3</code> <br>'
         )
       )
       .addDropdown(async (dropdown) => {
+        dropdown.addOption('list', 'List');
         dropdown.addOption('csv', 'CSV');
         dropdown.addOption('hashtag', 'Hashtags');
         dropdown.setValue(this.plugin.settings.tagFormat);
